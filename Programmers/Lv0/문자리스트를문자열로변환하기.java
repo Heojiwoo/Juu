d08fa613b0ef8a9e214ecb0443a9159263338011
Heojiwoo <anna001030@naver.com>
2024-03-19 22:34:22 +0900

    Commit

---
+++ Programmers/Lv0/문자리스트를문자열로변환하기.java
@@ -0,0 +1,8 @@
+package Programmers.Lv0;
+
+public class 문자리스트를문자열로변환하기 {
+    public String solution(String[] arr) {
+        String answer = String.join("", arr);
+        return answer;
+    }
+}
